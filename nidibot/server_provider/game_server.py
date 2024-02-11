88c20767125495bf78d333fcaf57fb20097bcdc7
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2024-02-11 21:31:06 +0200

    game_server.py: added docstrings.

--- nidibot/server_provider/game_server.py
+++ nidibot/server_provider/game_server.py
@@ -7,6 +7,10 @@ from nidibot.server_provider.server_provider_interface import (
 
 
 class GameServer:
+    """
+    A wrapper class for easier management of single game server instance.
+    """
+
     def __init__(
         self, server_provider: ServerProviderInterface, server_id: str, server_name: str
     ) -> None:
@@ -18,22 +22,64 @@ class GameServer:
         self.__server_name = server_name
 
     def name(self) -> str:
+        """
+        States server name.
+
+        Returns:
+            server_name (str)
+        """
         return self.__server_name
 
     def status(self) -> ServerStatus:
+        """
+        Returns server name.
+
+        Returns:
+            server_status (ServerStatus): server status as a structure
+        """
         return self.__server_provider.status(self.__server_id)
 
     def start(self) -> bool:
+        """
+        Requests server provider to start server.
+
+        Returns:
+            result (bool): operation result
+        """
         return self.__server_provider.start(self.__server_id)
 
     def stop(self) -> bool:
+        """
+        Requests server provider to stop server.
+
+        Returns:
+            result (bool): operation result
+        """
         return self.__server_provider.stop(self.__server_id)
 
     def restart(self) -> bool:
+        """
+        Requests server provider to restart server.
+
+        Returns:
+            result (bool): operation result
+        """
         return self.__server_provider.restart(self.__server_id)
 
     def create_backup(self) -> bool:
+        """
+        Requests server provider to create a game server backup.
+
+        Returns:
+            result (bool): operation result
+        """
         return self.__server_provider.create_backup(self.__server_id)
 
     def restore_backup(self) -> bool:
+        """
+        Requests server provider to restore a game server backup.
+
+        Returns:
+            result (bool): operation result
+        """
         return self.__server_provider.restore_backup(self.__server_id)
