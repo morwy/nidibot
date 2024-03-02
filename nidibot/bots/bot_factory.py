90f79bddb3bd1ef8fad91ee150c15c7eec25c3da
Pavel Ivanov <64430090+morwy@users.noreply.github.com>
2024-03-02 15:37:41 +0200

    refactor(bot_factory.py): changed support bot list to be a static private class field.

--- nidibot/bots/bot_factory.py
+++ nidibot/bots/bot_factory.py
@@ -9,20 +9,20 @@ from nidibot.server_provider.game_server import GameServer
 
 
 class BotFactory:
+    __supported_bots = {
+        "discord": DiscordBot,
+        "telegram": TelegramBot,
+    }
+
     @staticmethod
     def create(configuration: BotConfiguration, game_servers: List[GameServer]):
-        bots = {
-            "discord": DiscordBot,
-            "telegram": TelegramBot,
-        }
-
-        if configuration.type not in bots:
+        if configuration.type not in BotFactory.__supported_bots:
             if not configuration.type:
                 raise ValueError("Empty bot type provided!")
 
             raise ValueError("Unknown bot type provided!")
 
-        return bots[configuration.type](
+        return BotFactory.__supported_bots[configuration.type](
             configuration=configuration, game_servers=game_servers
         )
 
